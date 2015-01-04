01e34b798561ebd6f9bd6cb0bd54908923c86b8d
Eddie Sundvall <eddie.sundvall@gmail.com>
2015-01-04 18:13:45 +0100

    Adds function to set accelerometer full scale range setting

--- mpu9150.py
+++ mpu9150.py
@@ -1,7 +1,14 @@
 class mpu9150:
+    # Accelerometer addresses
+    ACCEL_CONFIG = 0x1c
     ACCEL_XOUT_L = 0x3b
     ACCEL_YOUT_L = 0x3d
     ACCEL_ZOUT_L = 0x3f
+    # Accelerometer values
+    AFS_SEL_2G = 0x0
+    AFS_SEL_4G = 0x1
+    AFS_SEL_8G = 0x2
+    AFS_SEL_16G = 0x3
 
     def __init__(self, i2c_bus):
         self.i2c = i2c_bus
@@ -14,3 +21,9 @@ class mpu9150:
 
     def read_z_acc(self):
         return self.i2c.read_word(self.ACCEL_ZOUT_L)
+
+    def set_acc_full_scale_range(self, value):
+        current_value = self.i2c.read_byte(self.ACCEL_CONFIG)
+        new_value = current_value & 0xe7
+        new_value |= value << 3
+        self.i2c.write_byte(self.ACCEL_CONFIG, new_value)
--- tests/test_mpu9150.py
+++ tests/test_mpu9150.py
@@ -1,7 +1,5 @@
 import unittest
 
-from mock import MagicMock
-
 from mpu9150 import mpu9150
 from i2c_interface import i2c_interface
 
@@ -12,19 +10,61 @@ class TestMPU9150(unittest.TestCase):
         self.imu = mpu9150(self.i2c)
 
     def testReadAccX(self):
-        self.i2c.read_word = MagicMock(return_value=0x39bc)
+        # Override read word functions in I2C I/F
+        # with a little bit of logic
+        def read_word(addr):
+            return 0x39bc if addr == mpu9150.ACCEL_XOUT_L else 0
+
+        self.i2c.read_word = read_word
+
         value = self.imu.read_x_acc()
         self.assertEquals(value, 0x39bc)
 
     def testReadAccY(self):
-        self.i2c.read_word = MagicMock(return_value=0x48e3)
+        # Override read word functions in I2C I/F
+        # with a little bit of logic
+        def read_word(addr):
+            return 0x48e3 if addr == mpu9150.ACCEL_YOUT_L else 0
+
+        self.i2c.read_word = read_word
+
         value = self.imu.read_y_acc()
         self.assertEquals(value, 0x48e3)
 
     def testReadAccZ(self):
-        self.i2c.read_word = MagicMock(return_value=0x5a3f)
+        # Override read word functions in I2C I/F
+        # with a little bit of logic
+        def read_word(addr):
+            return 0x5a3f if addr == mpu9150.ACCEL_ZOUT_L else 0
+
+        self.i2c.read_word = read_word
+
         value = self.imu.read_z_acc()
         self.assertEquals(value, 0x5a3f)
+
+    def testSetAccFullScaleRange(self):
+        self.i2c.value = 0xff
+        self.i2c.addr = 0x0000
+
+        # Override read and write byte functions in I2C I/F
+        # with a little bit of logic
+        def write_byte(addr, value):
+            self.i2c.addr = addr
+            self.i2c.value = value
+
+        self.i2c.write_byte = write_byte
+
+        def read_byte(addr):
+            if addr == mpu9150.ACCEL_CONFIG:
+                return self.i2c.value
+            else:
+                return 0
+
+        self.i2c.read_byte = read_byte
+
+        self.imu.set_acc_full_scale_range(mpu9150.AFS_SEL_4G)
+        self.assertEquals(self.i2c.addr, mpu9150.ACCEL_CONFIG)
+        self.assertEquals(self.i2c.value, 0xef)
 
 
 if __name__ == "__main__":
