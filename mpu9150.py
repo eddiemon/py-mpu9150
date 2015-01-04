1e6b5f8cd4a6a034ba43237784cd5ef561aff028
Eddie Sundvall <eddie.sundvall@gmail.com>
2015-01-04 20:37:23 +0100

    Adds function to set sample rate divider. Replaces I2C interface mock with a fake memory model implementation.

--- mpu9150.py
+++ mpu9150.py
@@ -10,20 +10,44 @@ class mpu9150:
     AFS_SEL_8G = 0x2
     AFS_SEL_16G = 0x3
 
+    SMPRT_DIV = 0x25
+
     def __init__(self, i2c_bus):
         self.i2c = i2c_bus
 
-    def read_x_acc(self):
+    @staticmethod
+    def twos_comp(val, bits):
+        """compute the 2's compliment of int value val"""
+        if( (val&(1<<(bits-1))) != 0 ):
+            val = val - (1<<bits)
+        return val
+
+    def read_x_acc_raw(self):
         return self.i2c.read_word(self.ACCEL_XOUT_L)
 
-    def read_y_acc(self):
+    def read_x_acc(self):
+        raw_value = self.read_x_acc_raw()
+        return mpu9150.twos_comp(raw_value, 16)
+
+    def read_y_acc_raw(self):
         return self.i2c.read_word(self.ACCEL_YOUT_L)
 
-    def read_z_acc(self):
+    def read_y_acc(self):
+        raw_value = self.read_y_acc_raw()
+        return mpu9150.twos_comp(raw_value, 16)
+
+    def read_z_acc_raw(self):
         return self.i2c.read_word(self.ACCEL_ZOUT_L)
+
+    def read_z_acc(self):
+        raw_value = self.read_z_acc_raw()
+        return mpu9150.twos_comp(raw_value, 16)
 
     def set_acc_full_scale_range(self, value):
         current_value = self.i2c.read_byte(self.ACCEL_CONFIG)
         new_value = current_value & 0xe7
         new_value |= value << 3
         self.i2c.write_byte(self.ACCEL_CONFIG, new_value)
+
+    def set_sample_rate_divider(self, value):
+        self.i2c.write_byte(self.SMPRT_DIV, value)
--- tests/test_mpu9150.py
+++ tests/test_mpu9150.py
@@ -5,66 +5,77 @@ from i2c_interface import i2c_interface
 
 
 class TestMPU9150(unittest.TestCase):
+
+    class i2cMock(i2c_interface):
+        '''Mock class for I2C interface. Creates a dictionary that represents
+        memory which can be read and written from. All values in one "address"
+        in the dictionary shall be 8 bits.'''
+        memory = dict()
+
+        def read_byte(self, addr):
+            return self.memory.get(str(addr), 0)
+
+        def read_word(self, addr):
+            return self.read_byte(addr) + (self.read_byte(addr + 1) << 8)
+
+
+        def write_byte(self, addr, value):
+            self.memory[str(addr)] = value & 0xff
+
+        def write_word(self, addr, value):
+            self.memory[str(addr)] = value & 0xff
+            self.memory[str(addr+1)] = (value >> 8) & 0xff
+
     def setUp(self):
-        self.i2c = i2c_interface()
+        self.i2c = self.i2cMock()
         self.imu = mpu9150(self.i2c)
 
-    def testReadAccX(self):
-        # Override read word functions in I2C I/F
-        # with a little bit of logic
-        def read_word(addr):
-            return 0x39bc if addr == mpu9150.ACCEL_XOUT_L else 0
+    def testReadAccXRaw(self):
+        self.i2c.write_word(mpu9150.ACCEL_XOUT_L, 0x39bc)
 
-        self.i2c.read_word = read_word
-
-        value = self.imu.read_x_acc()
+        value = self.imu.read_x_acc_raw()
         self.assertEquals(value, 0x39bc)
 
-    def testReadAccY(self):
-        # Override read word functions in I2C I/F
-        # with a little bit of logic
-        def read_word(addr):
-            return 0x48e3 if addr == mpu9150.ACCEL_YOUT_L else 0
+    def testReadAccX(self):
+        self.i2c.write_word(mpu9150.ACCEL_XOUT_L, 0xf830)
 
-        self.i2c.read_word = read_word
+        value = self.imu.read_x_acc()
+        self.assertEquals(value, -2000)
 
-        value = self.imu.read_y_acc()
+    def testReadAccYRaw(self):
+        self.i2c.write_word(mpu9150.ACCEL_YOUT_L, 0x48e3)
+
+        value = self.imu.read_y_acc_raw()
         self.assertEquals(value, 0x48e3)
 
-    def testReadAccZ(self):
-        # Override read word functions in I2C I/F
-        # with a little bit of logic
-        def read_word(addr):
-            return 0x5a3f if addr == mpu9150.ACCEL_ZOUT_L else 0
+    def testReadAccY(self):
+        self.i2c.write_word(mpu9150.ACCEL_YOUT_L, 0xf830)
 
-        self.i2c.read_word = read_word
+        value = self.imu.read_y_acc()
+        self.assertEquals(value, -2000)
 
-        value = self.imu.read_z_acc()
+    def testReadAccZRaw(self):
+        self.i2c.write_word(mpu9150.ACCEL_ZOUT_L, 0x5a3f)
+
+        value = self.imu.read_z_acc_raw()
         self.assertEquals(value, 0x5a3f)
 
+    def testReadAccZ(self):
+        self.i2c.write_word(mpu9150.ACCEL_ZOUT_L, 0xf830)
+
+        value = self.imu.read_z_acc()
+        self.assertEquals(value, -2000)
+
     def testSetAccFullScaleRange(self):
-        self.i2c.value = 0xff
-        self.i2c.addr = 0x0000
-
-        # Override read and write byte functions in I2C I/F
-        # with a little bit of logic
-        def write_byte(addr, value):
-            self.i2c.addr = addr
-            self.i2c.value = value
-
-        self.i2c.write_byte = write_byte
-
-        def read_byte(addr):
-            if addr == mpu9150.ACCEL_CONFIG:
-                return self.i2c.value
-            else:
-                return 0
-
-        self.i2c.read_byte = read_byte
+        self.i2c.write_byte(mpu9150.ACCEL_CONFIG, 0xff)
 
         self.imu.set_acc_full_scale_range(mpu9150.AFS_SEL_4G)
-        self.assertEquals(self.i2c.addr, mpu9150.ACCEL_CONFIG)
-        self.assertEquals(self.i2c.value, 0xef)
+        self.assertEquals(self.i2c.read_byte(mpu9150.ACCEL_CONFIG), 0xef)
+
+    def testSetSampleRateDivider(self):
+        self.imu.set_sample_rate_divider(0x3c)
+        self.assertEqual(self.i2c.read_byte(mpu9150.SMPRT_DIV), 0x3c)
+
 
 
 if __name__ == "__main__":
